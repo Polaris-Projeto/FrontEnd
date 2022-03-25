2f944a0a0e756ecc13d9dcf2ff96ea17cb470e12
Rodrigo Pereira <rodrigopalexandre1@hotmail.com>
2022-03-25 10:00:49 -0300

    Inserindo Front End

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+          
+            <AppBar position="static">
+                <div id="barra">
+                    <Toolbar variant="dense">
+                        <Box style={{ cursor: "pointer" }} >
+                            <Typography variant="h5" color="inherit">
+                                BlogPessoal
+                            </Typography>
+                        </Box>
+
+                        <Box display="flex" justifyContent="start">
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Home
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Postagens
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar Tema
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Box>
+
+                    </Toolbar>
+                </div>
+            </AppBar>
+        
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Grid, Box} from '@material-ui/core';
+import './Login.css';
+
+function Login(){
+
+    return(
+        <Grid>
+            <Grid xs={6}>
+                <Box>
+                    <form action="">
+
+                    </form>
+                </Box>
+            </Grid>
+            <Grid xs={6}>
+
+            </Grid>
+        </Grid>
+    );
+}
+
+
+export default Login;
