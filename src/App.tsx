5a0492b43643a25baa85e44adbe0dc6ad4d4d928
Rodrigo Pereira <rodrigopalexandre1@hotmail.com>
2022-04-05 10:09:05 -0300

    Adicionando Crud Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,81 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import Home from "./paginas/home/Home";
-import Login from "./paginas/login/Login";
-import Contato from "./paginas/contato/Contato";
-import './App.css';
-import Sobre from './paginas/sobre/Sobre';
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import './App.css';
+import ListaTema from './components/temas/listaTema/ListaTema';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-
       <Switch>
-        <div style={{ minHeight: "100vh" }}>
-          <Route exact path="/">
+        <div style={{ minHeight: '100vh' }}>
+          <Route exact path='/'>
             <Login />
           </Route>
-          <Route path="/sobre">
-            <Sobre />
-          </Route>
-          <Route path="/login">
+
+          <Route path='/login'>
             <Login />
           </Route>
-          <Route path="/home">
+
+          <Route path='/home'>
             <Home />
           </Route>
-          <Route path="/contato">
-            <Contato />
-          </Route>
-          <Route path="/cadastrousuario">
+
+          <Route path='/cadastrousuario'>
             <CadastroUsuario />
-          </Route>                  
-        
+          </Route>
+
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
+
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
+
+          <Route exact path='/formularioPostagem'>
+            <CadastroPost />
+          </Route>
+
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPost />
+          </Route>
+
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
+          
         </div>
       </Switch>
-
       <Footer />
     </Router>
   );
