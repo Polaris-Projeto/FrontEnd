fb7bf60598bab34d3cd40cb0f64c5f0f2e0273fc
Leonardo0908 <leonardo.elias0908@gmail.com>
2022-04-19 12:43:39 -0300

    atualizando home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -37,29 +37,24 @@ function Home() {
     }, [token])
     return (
         <>
-            <Grid xs={12} container direction="row" className='caixaPrincipal'>
-                <Grid item xs={3} className='usuario' alignItems="center">
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                            <ModalPostagem />
-                        </Box>
-                        <Link to="/posts" className='botao' >
-                            <Fab variant="extended" className='botao'>Ver Postagens</Fab>
-                        </Link>
+        <Grid xs={12} container direction="row" className='caixaPrincipal'>
+            <Grid item xs={3} className='usuario' alignItems="center">
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
+                        <ModalPostagem />
                     </Box>
-                </Grid>
-                <Grid item xs={6} >
-                  
-                </Grid>
-                <Grid xs={12} className='postagens'>
-
-                    <TabPostagem />
-                </Grid>
+                    <Link to="/posts" className='botao' >
+                        <Fab variant="extended" className='botao'>Ver Postagens</Fab>
+                    </Link>
+                </Box>
             </Grid>
+            <Grid item xs={8}>
+                <TabPostagem />
+            </Grid>
+        </Grid>
 
 
-        </>
-
+    </>
     );
 }
 export default Home;
